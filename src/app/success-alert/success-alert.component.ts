57606039478907256484f778ab64fac7042da5dc
Krystian Kowalik <krystiankowalik1991@gmail.com>
2019-03-04 15:05:36 +0100

    Initial commit

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: './success-alert.component.html',
+  selector: 'app-success-alert',
+  styleUrls: ['./success-alert.component.css']
+})
+export class SuccessAlertComponent {
+
+}
